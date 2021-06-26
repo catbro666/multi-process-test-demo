d577f870293f34523fd70006ce14375b346d173d
Zhefeng Chen <chzf_zju@163.com>
2021-06-26 18:20:12 +0800

    prevent shm leakage, add final result

--- multi-process.c
+++ multi-process.c
@@ -1,8 +1,8 @@
 #include <stdio.h>      // printf, fprintf
 #include <sys/wait.h>   // wait
 #include <sys/types.h>  // getpid, wait
-#include <sys/ipc.h>    // shmget, shmctl
-#include <sys/shm.h>    // shmget, shmctl
+#include <sys/ipc.h>    // shmget, shmat, shmctl, shmdt
+#include <sys/shm.h>    // shmget, shmat, shmctl, shmdt
 #include <signal.h>     // sigaction, SIGLARM
 #include <limits.h>     // LONG_MIN, LONG_MAX, ULLONG_MAX
 #include <errno.h>      // errno
@@ -40,7 +40,7 @@ void handle_signal_parent(int sigNum)
         for (long i = 0; i < opt.procs; ++i) {
             res_total.count += shm[i].count;
         }
-        fprintf(stderr, "total count %12llu, average %12.0lf\n",
+        fprintf(stderr, "total count %12llu,  average %12.0lf/s\n",
                 res_total.count, (res_total.count - res_last.count)
                 / (double)opt.interval);
         memcpy(&res_last, &res_total, sizeof(Result));
@@ -58,8 +58,6 @@ void doTest(void *param) {
         ++shm[pa->index].count;
         /* DO YOUR WORK */
     }
-    fprintf(stderr, "process [pid = %6u] result: %llu\n",
-           getpid(), shm[pa->index].count);
 }
 
 int main(int argc, char *argv[]) {
@@ -71,8 +69,6 @@ int main(int argc, char *argv[]) {
     pid_t pid = 0;
     struct sigaction act_child;
     struct sigaction act_parent;
-    struct sigaction act_quit;
-    struct sigaction act_int;
 
     rv = process_options(argc, argv, &opt);
     if (rv) {
@@ -88,12 +84,14 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "shmget() failed\n");
         return -1;
     }
-    //fprintf(stderr, "shmid = %d\n", shmid);
+    fprintf(stderr, "shmid = %d\n", shmid);
     shm = (Result*)shmat(shmid, 0, 0);
     if ((void *) -1 == shm) {
         fprintf(stderr, "shmat() failed\n");
         return -1;
     }
+    /* 这里直接进行IPC_RMID操作，进程退出之后会自动detach了, 从而释放共享内存 */
+    shmctl(shmid, IPC_RMID, 0);
     memset(shm, 0, sizeof(sizeof(Result) * opt.procs));
     /* COMMON INIT */
 
@@ -122,44 +120,27 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "sigaction() failed\n");   
             return -1;
         }
-#if 0
-        act_quit.sa_handler = handle_signal_exit;
-        sigemptyset(&act_quit.sa_mask);
-        sigaddset(&act_quit.sa_mask, SIGQUIT);
-        sigaddset(&act_quit.sa_mask, SIGINT);
-        act_quit.sa_flags = SA_RESETHAND;
-        rv = sigaction(SIGALRM, &act_quit, NULL);      // 捕获SIGQUIT信号，终止前释放共享内存
-        if (rv) {
-            fprintf(stderr, "sigaction() failed\n");
-            return -1;
-        }
-
-        act_int.sa_handler = handle_signal_exit;
-        sigemptyset(&act_int.sa_mask);
-        sigaddset(&act_quit.sa_mask, SIGQUIT);
-        sigaddset(&act_quit.sa_mask, SIGINT);
-        act_quit.sa_flags = SA_RESETHAND;
-        rv = sigaction(SIGALRM, &act_int, NULL);      // 捕获SIGINT信号，终止前释放共享内存
-        if (rv) {
-            fprintf(stderr, "sigaction() failed\n");
-            return -1;
-        }
-#endif
         memset(&res_last, 0, sizeof(Result));
         alarm(opt.interval);
         /* PARENT INIT */
+        /* DO FINAL STATISTICS */
+        Result final;
+        memset(&final, 0, sizeof(Result));
         for(i =0 ; i < opt.procs; ++i) {
             pid = wait(&wstatus);                       // 等待子进程结束
             alarm(0);                                   // 终止定时器
             if(pid == -1) {
                 fprintf(stderr, "wait() failed, errno=%d\n", errno);
+                return -1;
             }
             fprintf(stderr, "process [pid = %6d] exit\n", pid);
+            fprintf(stderr, "process [pid = %6u] count %12llu in %lus,  average %12.0lf/s\n", 
+                   pid, shm[i].count, opt.duration, shm[i].count / (double)opt.duration);
+            final.count += shm[i].count;
         }
-        
-        shmdt((void*)shm);
-        /* 子进程退出之后自动detach了, 所以这里不需要通过IPC_STAT进行判断 */
-        shmctl(shmid, IPC_RMID, 0);
+        fprintf(stderr, "total count %12llu in %lus,  average %12.0lf/s\n", 
+               final.count, opt.duration, final.count / (double)opt.duration);
+        /* DO FINAL STATISTICS */
     }
     else {
         /* CHILD INIT */
